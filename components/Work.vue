2a8c202c85813322042f984b16048d1258894603
jamesmwangi55 <jamesmwangi55@gmail.com>
2018-06-16 11:01:24 +0300

    Add basic card to to Work

--- components/Work.vue
+++ components/Work.vue
@@ -1,10 +1,44 @@
 <template>
-    
+    <div
+            id="e3"
+            style="max-width: 400px; margin: auto;"
+            class="grey lighten-3"
+    >
+        <v-toolbar color="pink">
+            <v-toolbar-side-icon></v-toolbar-side-icon>
+            <v-toolbar-title class="white--text">My Music</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn icon>
+                <v-icon>search</v-icon>
+            </v-btn>
+        </v-toolbar>
+        <v-card>
+            <v-container
+                    fluid
+                    style="min-height: 0;"
+                    grid-list-lg
+            >
+                <v-layout row wrap>
+                    <v-flex xs12>
+                        <v-card color="blue-grey darken-2" class="white--text">
+                            <v-card-title primary-title>
+                                <div class="headline">Unlimited music now</div>
+                                <div>Listen to your favorite artists and albums whenever and wherever, online and offline.</div>
+                            </v-card-title>
+                            <v-card-actions>
+                                <v-btn flat dark>Listen now</v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </v-card>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "MyWork"
+      name: 'Work'
     }
 </script>
 
