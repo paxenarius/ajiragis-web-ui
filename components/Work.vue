8c5ca9354ffb1cd6e85037f526c37993862a2149
jamesmwangi55 <jamesmwangi55@gmail.com>
2018-06-16 12:22:24 +0300

    Correctly layout card component

--- components/Work.vue
+++ components/Work.vue
@@ -4,29 +4,41 @@
             style="max-width: 400px; margin: auto;"
             class="grey lighten-3"
     >
-        <v-toolbar color="pink">
-            <v-toolbar-side-icon></v-toolbar-side-icon>
-            <v-toolbar-title class="white--text">My Music</v-toolbar-title>
+        <v-toolbar color="grey lighten-2">
+            <!--<v-toolbar-side-icon></v-toolbar-side-icon>-->
+            <v-toolbar-title class="black--text">Work</v-toolbar-title>
             <v-spacer></v-spacer>
-            <v-btn icon>
-                <v-icon>search</v-icon>
-            </v-btn>
         </v-toolbar>
         <v-card>
             <v-container
                     fluid
-                    style="min-height: 0;"
-                    grid-list-lg
+                    style="min-height: 0; padding: 0 !important;"
             >
                 <v-layout row wrap>
                     <v-flex xs12>
-                        <v-card color="blue-grey darken-2" class="white--text">
-                            <v-card-title primary-title>
-                                <div class="headline">Unlimited music now</div>
-                                <div>Listen to your favorite artists and albums whenever and wherever, online and offline.</div>
-                            </v-card-title>
+                        <v-card color="white" light>
+                            <v-card-text>
+                                <v-container fluid grid-list-md>
+                                    <v-layout row >
+                                        <v-flex d-flex xs12 sm6 md4>
+                                            <v-icon large color="green">bar_chart</v-icon>
+                                        </v-flex>
+                                        <v-flex d-flex xs12 sm6 md3>
+                                            <v-layout row wrap>
+                                                <v-flex d-flex>
+                                                    <span class="headline grey--text text--darken-1">500</span>
+                                                </v-flex>
+                                                <v-flex d-flex>
+                                                    <span class="grey--text text--lighten-1">Translations</span>
+                                                </v-flex>
+                                            </v-layout>
+                                        </v-flex>
+                                    </v-layout>
+                                </v-container>
+                            </v-card-text>
                             <v-card-actions>
-                                <v-btn flat dark>Listen now</v-btn>
+                                <v-spacer></v-spacer>
+                                <v-btn dark color="green">Request Work</v-btn>
                             </v-card-actions>
                         </v-card>
                     </v-flex>
@@ -38,7 +50,10 @@
 
 <script>
     export default {
-      name: 'Work'
+      name: 'Work',
+      data: () => ({
+        lorem: `Lorem ipsum dolor sit amet, mel at clita quando. Te sit oratio vituperatoribus, nam ad ipsum posidonium mediocritatem, explicari dissentiunt cu mea. Repudiare disputationi vim in, mollis iriure nec cu, alienum argumentum ius ad. Pri eu justo aeque torquatos.`
+      })
     }
 </script>
 
