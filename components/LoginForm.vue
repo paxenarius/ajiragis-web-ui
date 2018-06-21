aa74f889707e694e83af30d344414517be745591
jamesmwangi55 <jamesmwangi55@gmail.com>
2018-06-22 00:46:41 +0300

    Add login form and logo

---
+++ components/LoginForm.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-layout>
+        <v-flex text-xs-center xs12 sm6 offset-sm3>
+            <h1>Sign In With Google</h1>
+            <v-btn color="error" dark large @click.native="googleSignUp">Google</v-btn>
+            <!--<v-btn color="info" dark large>Facebook</v-btn>-->
+        </v-flex>
+    </v-layout>
+</template>
+
+<script>
+    export default {
+      name: 'LoginForm',
+      methods: {
+        googleSignUp () {
+          this.$store.dispatch('signInWithGoogle').then(() => {
+            console.log('inside then statement on login')
+          }).catch((e) => {
+            console.log(e.message)
+          })
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
