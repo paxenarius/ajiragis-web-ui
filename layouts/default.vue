0f4811479a6851bde6708436044b1e843e9a855c
bmarika <mwaura.marika@gmail.com>
2018-09-13 15:21:12 +0300

    Fix the BASE_URL to always point to localhost. Add DS_STORE to gitignore.

--- layouts/default.vue
+++ layouts/default.vue
@@ -146,30 +146,31 @@
         fixed: false,
         items: [
           { icon: 'apps', title: 'Welcome', to: '/' },
-          [
-            {
-              icon: 'input',
-              title: 'Data-Collector',
-              to: '/Data-Collector',
-              toggle_on: 'dataCollectorClick'
-            },
-            {
-              icon: 'input',
-              title: 'Contribute',
-              to: '/contribute',
-              toggle_on: 'dataCollectorClick'
-            },
-            {
-              icon: 'table',
-              title: 'Contributions',
-              to: '/contributionslist',
-              toggle_on: 'dataCollectorClick'
-            }
-          ],
           { icon: 'compare_arrows', title: 'Translate', to: '/translations/translate' },
           { icon: 'compare_arrows', title: 'Translations', to: '/translations/translations' },
           { icon: 'compare_arrows', title: 'Collect', to: '/collect/contribute' },
           { icon: 'compare_arrows', title: 'Contributions', to: '/collect/contributions' }
+          // [
+          //   {
+          //     icon: 'input',
+          //     title: 'Data-Collector',
+          //     to: '/Data-Collector',
+          //     toggle_on: 'dataCollectorClick'
+          //   },
+          //   {
+          //     icon: 'input',
+          //     title: 'Contribute',
+          //     to: '/contribute',
+          //     toggle_on: 'dataCollectorClick'
+          //   },
+          //   {
+          //     icon: 'table',
+          //     title: 'Contributions',
+          //     to: '/contributionslist',
+          //     toggle_on: 'dataCollectorClick'
+          //   }
+          // ],
+
         ],
         submenuToggles: {dataCollectorClick:false},
         miniVariant: false,
